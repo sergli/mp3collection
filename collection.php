f976ea75550923f31d40468ff5ceb1fcba7cbec6
Sergey Lisenkov <s.lisenkov@corp.badoo.com>
2018-03-13 01:03:00 +0300

    die on incorrect usage

--- collection.php
+++ collection.php
@@ -2,9 +2,15 @@
 
 include 'vendor/autoload.php';
 
-$pdo = new PDO('mysql:host=localhost;dbname=music', 'root', '12345', [ PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'	]);
+$pdo = new PDO('mysql:host=localhost;dbname=music', 'root', '12345', [ PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8']);
 
-$Col = new \Mp3\Collection('/ipv-OK/', 2);
+if ($argc <= 2) {
+	exit(1);
+}
+$path = $argv[1];
+$slice_parts = $argv[2];
+
+$Col = new \Mp3\Collection($path, $slice_parts);
 
 $Mapper = new \Mp3\FileMapper($pdo);
 $all = $Mapper->getAll();
